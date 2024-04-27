99faf8bcc214f908a1d21094945b1f8ddef272e3
Zoheb <zohebcool1542@gmail.com>
2024-04-27 12:55:32 +0530

    error page added

--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  HashRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
 import Services from "./pages/Services.jsx";
 import JobsListing from "./pages/JobsListing.jsx";
+import Error from "./pages/Error.jsx";
 const router = createBrowserRouter([
   {
-    path: "/alahyan/",
+    path: "/alahyan",
     element: <App />,
-  },
-  {
-    path: "/alahyan/about",
-    element: <About />,
-  },
+    errorElement: <Error />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
 
-  {
-    path: "/alahyan/services",
-    element: <Services />,
-  },
-  {
-    path: "/alahyan/contact",
-    element: <Contact />,
-  },
-  {
-    path: "/alahyan/jobs",
-    element: <JobsListing />,
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "jobs",
+        element: <JobsListing />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    {/* <HashRouter basename="/alahyan">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/jobs" element={<JobsListing />} />
+      </Routes>
+    </HashRouter> */}
     <RouterProvider router={router} />
   </React.StrictMode>
 );
